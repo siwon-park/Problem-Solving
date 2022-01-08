17f7a03bcab63dd65fc9a29181e6bb282d98f574
박시원 <93081720+siwon-park@users.noreply.github.com>
2022-01-08 13:36:20 +0900

    Create 11722.py

---
+++ DP/11722.py
@@ -0,0 +1,21 @@
+#가장 긴 감소하는 부분 수열(11722번)
+##################################################
+    # 문제: https://www.acmicpc.net/problem/11722
+    # DP(다이나믹 프로그래밍)
+    # 가장 긴 증가하는 부분 수열(11053번)과 동일한 문제
+    # 다만 가장 긴 증가하는 부분 수열 문제는 j가 i보다 커져야해서 for j in range(N+1) 이지만,
+    # 이 문제는 가장 긴 감소하는 부분 수열이므로, j가 i보다 작은 부분까지 가야한다.
+##################################################
+import sys
+input=sys.stdin.readline
+N=int(input())
+A=list(map(int,input().split()))
+dp=[0]*1000
+answer=1
+for i in range(N):
+    dp[i]=1
+    for j in range(i):
+        if A[j]>A[i]:
+            dp[i]=max(dp[j]+1,dp[i])
+    answer=max(answer,dp[i])
+print(answer)
