532ad29509f1381ccfc5df527a7803acb7a7aa43
박시원 <93081720+siwon-park@users.noreply.github.com>
2022-01-13 09:02:39 +0900

    Create AB.java

---
+++ AB.java
@@ -0,0 +1,11 @@
+//A+B(1000번)
+import java.util.*;
+public class AB {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in); //이게 자바에서 백준 인풋 받는 것과 같은 것 같음 input=sys.stdin.readline
+        int A, B;
+        A= sc.nextInt();
+        B= sc.nextInt();
+        System.out.println(A+B);
+    }
+}
