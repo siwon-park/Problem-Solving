9b733534e4c7b3c1c49231930bd50d830c0b8d41
박시원 <zow777@naver.com>
2022-01-21 17:46:44 +0900

    Create 10869.java

---
+++ Java_BOJ/10869.java
@@ -0,0 +1,19 @@
+//사칙연산(10869번)
+///////////////////////////////////////////////////
+    // 문제: https://www.acmicpc.net/problem/10869
+    // 수학, 사칙연산
+    // 그냥 풀면 됨
+///////////////////////////////////////////////////
+import java.util.*;
+public class Solution{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        System.out.println(A+B);
+        System.out.println(A-B);
+        System.out.println(A*B);
+        System.out.println(A/B);
+        System.out.println(A%B);
+    }
+}
