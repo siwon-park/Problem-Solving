5157c595e0e1c8d2b228d4a21752c0da9b64f020
박시원 <zow777@naver.com>
2022-02-03 23:41:24 +0900

    Create 18108.java

---
+++ Java_BOJ/18108.java
@@ -0,0 +1,14 @@
+// 1998년생인 내가 태국에서는 2541년생?!(18108번)
+////////////////////////////////////////////////
+  // 문제: https://www.acmicpc.net/problem/18108
+  // 수학
+  // 잔디 심기용
+////////////////////////////////////////////////
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int y = sc.nextInt();
+        System.out.println(y-543);
+    }
+}
