486da14292047ee239e4e0e4011eee8df2f5f69b
박시원 <zow777@naver.com>
2022-04-07 23:25:13 +0900

    Update 17471.py

--- DFS/17471.py
+++ DFS/17471.py
@@ -7,7 +7,8 @@
     # 여기서 말하는 중복없음이란 [0], [1, 2, 3, 4, 5] 일 때, [1, 2, 3, 4, 5], [0] 이렇게 서로 대칭되는 구조가 나오지 않는 것을 말한다.
     # 이 두 개는 결과가 같아서 사실상 같으므로 1개로 쳐야한다.
     # 그리고 두 구역의 개수의 합이 N개 일 때, BFS함수를 돌려서, 구역과 그 구역에 대한 합을 구한다.
-    # 구역의 크기가 1로 나와야만 해당 구역은 서로 연결되어 있는 구역이다. 2이상이 나오면 어딘가 떨어져 있다는 의미임
+    # 구역의 크기가 1로 나와야만 해당 구역은 서로 연결되어 있는 구역이다. 2 이상이 나오면 어딘가 떨어져 있다는 의미이다.
+    # 따라서 A와 B 영역의 크기가 1일 때만 선거구의 합의 차이에 대한 최솟값을 구한다.
 ######################################################################
 import sys
 from collections import deque
@@ -26,10 +27,11 @@ for i in range(N):
 INF = sys.maxsize
 min_diff = sys.maxsize
 
+# BFS
 def bfs(lst):
-    area = 0
+    area = 0 # 구역의 크기
+    area_sum = 0 # 구역의 합
     visited = [False]*N
-    area_sum = 0
     for s in lst:
         if not visited[s]:
             visited[s] = True
@@ -39,13 +41,14 @@ def bfs(lst):
                 cur = q.popleft()
                 area_sum += regions[cur]
                 for nxt in graph[cur]:
-                    if not visited[nxt] and nxt in lst:
+                    if not visited[nxt] and nxt in lst: # 방문하지 않았으면서, 현재 같은 부분집합 셋에 있을 경우에만 방문
                         q.append(nxt)
                         visited[nxt] = True
-    return area, area_sum
+    return area, area_sum # 구역의 크기와 구역의 합을 반환
 
 A = [0] # A에 0을 넣고 부분집합을 구함으로써 중복을 제거할 수 있다.
 B = [] # 왜냐하면 A에 0부터 넣고 시작했으니, B에는 절대 0이 들어갈 수 없어서 0부터 시작해서 만드는 조합셋을 가질 수가 없기 때문이다.
+# 부분집합을 구하는 함수
 def subset(k):
     global min_diff
     if k == N:
@@ -53,7 +56,7 @@ def subset(k):
             return
         a1, sum1 = bfs(A)
         a2, sum2 = bfs(B)
-        if a1 == 1 and a2 == 1:
+        if a1 == 1 and a2 == 1: # 각 구역의 크기가 1일 때만 구역의 합의 차이의 최솟값을 계산 및 갱신한다.
             min_diff = min(min_diff, abs(sum1 - sum2))
         return
     else:
