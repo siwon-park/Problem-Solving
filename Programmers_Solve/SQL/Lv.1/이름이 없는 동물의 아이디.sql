f39d7213f7d6851d838a1b401493af3992d71535
박시원 <zow777@naver.com>
2022-08-21 11:28:21 +0900

    Create 이름이 없는 동물의 아이디.sql

---
+++ Programmers_Solve/SQL/Lv.1/이름이 없는 동물의 아이디.sql
@@ -0,0 +1,5 @@
+-- 동물 보호소에 들어온 동물 중, 이름이 없는 채로 들어온 동물의 ID를 조회하는 SQL 문을 작성해주세요. 단, ID는 오름차순 정렬되어야 합니다
+-- SELECT ANIMAL_ID FROM ANIMAL_INS WHERE NAME IS NULL;을 해도 맞다고 뜸
+-- NULL인지 NOT NULL인지 판별하기 위해서는 '칼럼명 IS NULL', '칼럼명 IS NOT NULL'로 쓴다.
+
+SELECT ANIMAL_ID FROM ANIMAL_INS WHERE NAME IS NULL ORDER BY ANIMAL_ID ASC;
