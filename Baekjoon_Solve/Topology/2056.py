f1f1cd8759177947d84c2df989194df500aa525a
박시원 <zow777@naver.com>
2022-10-01 19:57:28 +0900

    Update 2056.py

--- Baekjoon_Solve/Topology/2056.py
+++ Baekjoon_Solve/Topology/2056.py
@@ -47,3 +47,40 @@ while q:
             q.append((T[nxt], nxt))
 
 print(max(T))
+
+##########################################################################33
+# DP 풀이
+import sys
+input = sys.stdin.readline
+
+def calc_time(n):
+    global time, dp, first
+    if dp[n]: return dp[n]
+
+    if not first[n]:
+        dp[n] = time[n]
+        return dp[n]
+
+    maxx = 0
+    while first[n]:
+        nn = first[n].pop()
+        ret = calc_time(nn)
+        if maxx < ret:
+            maxx = ret
+
+    dp[n] = time[n] + maxx
+    return dp[n]
+
+N = int(input())
+dp = [0]*(N+1)
+time = [0]*(N+1)
+first = [set() for _ in range(N+1)]
+for i in range(1, N+1):
+    t, n, *arr = map(int, input().split())
+    time[i] = t
+    first[i] = set(arr)
+
+for i in range(1, N+1):
+    dp[i] = calc_time(i)
+
+print(max(dp))
