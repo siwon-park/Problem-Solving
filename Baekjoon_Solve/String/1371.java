df7033c9406f7db2e762fb73327d6456003bdaea
박시원 <zow777@naver.com>
2022-10-12 13:55:33 +0900

    Create 1371.java

---
+++ Baekjoon_Solve/String/1371.java
@@ -0,0 +1,43 @@
+// 가장 많은 글자(1371번)
+////////////////////////////////////////////////////////////////////////////////////////
+  // 문제: https://www.acmicpc.net/problem/1371
+  // 문자열, 구현
+////////////////////////////////////////////////////////////////////////////////////////
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int[] arr = new int[26];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String S = "";
+        while ((S = br.readLine()) != null) {
+            int N = S.length();
+            for (int i=0; i<N; i++) {
+                char s = S.charAt(i);
+                if ('a' <= s && s <= 'z') {
+                    arr[s - 'a'] ++;
+                }
+            }
+        }
+
+        int max = 0;
+        for (int i=0; i<26; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        String ans = "";
+        for (int i=0; i<26; i++) {
+            if (arr[i] == max) {
+                ans += (char)(i + 97) + "";
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
