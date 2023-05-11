348af1e8c89da65953be8f2822e6e091cbabf172
siwon-park <zow777@naver.com>
2023-05-12 06:42:25 +0900

    ps: 와이버스 부릉부릉

---
+++ Baekjoon_Solve/기타/14645/14645.py
@@ -0,0 +1,9 @@
+# 와이버스 부릉부릉 (14645번)
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().rstrip().split())
+for _ in range(N):
+    A, B = map(int, input().rstrip().split())
+
+print("비와이")
