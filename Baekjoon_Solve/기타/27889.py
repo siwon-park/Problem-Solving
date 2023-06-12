61e196a75a8185ad194428443ef80f99c9573671
PARK SIWON <zow777@naver.com>
2023-06-12 15:34:13 +0900

    ps: 특별한 학교 이름 (27889번) 풀이 완료

---
+++ Baekjoon_Solve/기타/27889.py
@@ -0,0 +1,13 @@
+# 특별한 학교 이름 (27889번)
+import sys
+input = sys.stdin.readline
+
+school_name = input().rstrip()
+if school_name == "NLCS":
+    print("North London Collegiate School")
+elif school_name == "BHA":
+    print("Branksome Hall Asia")
+elif school_name == "KIS":
+    print("Korea International School")
+elif school_name == "SJA":
+    print("St. Johnsbury Academy")
