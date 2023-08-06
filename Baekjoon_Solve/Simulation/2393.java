86574d521e8ac10cd16f07203a51e628309ccfbb
siwon-park <zow777@naver.com>
2023-08-06 19:06:17 +0900

    ps: Rook (2393번) 풀이 완료

---
+++ Baekjoon_Solve/Simulation/2393.java
@@ -0,0 +1,28 @@
+// Rook (2393번)
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+ 	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		bw.write("  ___  ___  ___" + "\n");
+		bw.write("  | |__| |__| |" + "\n");
+		bw.write("  |           |" + "\n");
+		bw.write("   \\_________/" + "\n");
+		bw.write("    \\_______/" + "\n");
+		bw.write("     |     |" + "\n");
+		bw.write("     |     |" + "\n");
+		bw.write("     |     |" + "\n");
+		bw.write("     |     |" + "\n");
+		bw.write("     |_____|" + "\n");
+		bw.write("  __/       \\__" + "\n");
+		bw.write(" /             \\" + "\n");
+		bw.write("/_______________\\");
+		bw.flush();
+		bw.close();
+		br.close();
+ 	}
+}
