28d097f0f36128393bb2b55692118ad7c9960c70
siwon-park <zow777@naver.com>
2023-10-06 18:15:14 +0900

    ps: 콜센터 (5339번) 풀이 완료

---
+++ Baekjoon_Solve/기타/5339.py
@@ -0,0 +1,14 @@
+# 콜센터 (5339번)
+import sys
+input = sys.stdin.readline
+
+print("     /~\\")
+print("    ( oo|")
+print("    _\=/_")
+print("   /  _  \\")
+print("  //|/.\|\\\\")
+print(" ||  \ /  ||")
+print("============")
+print("|          |")
+print("|          |")
+print("|          |")
