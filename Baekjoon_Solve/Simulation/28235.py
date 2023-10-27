07b9febd754477c733f616226d7cc4038c2df57b
siwon-park <zow777@naver.com>
2023-10-27 23:02:18 +0900

    ps: 코드마스터 2023 (28235번) 풀이 완료

---
+++ Baekjoon_Solve/Simulation/28235.py
@@ -0,0 +1,13 @@
+# 코드마스터 2023 (28235번)
+import sys
+input = sys.stdin.readline
+
+line = input().rstrip()
+if line == "SONGDO":
+    print("HIGHSCHOOL")
+elif line == "CODE":
+    print("MASTER")
+elif line == "2023":
+    print("0611")
+elif line == "ALGORITHM":
+    print("CONTEST")
