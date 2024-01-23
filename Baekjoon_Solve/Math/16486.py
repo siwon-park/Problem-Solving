db539acd617ee6b238ba2f33cb58c1abf7872a3f
siwon-park <zow777@naver.com>
2024-01-23 23:13:57 +0900

    ps: 운동장 한 바퀴 (16486번) 풀이 완료

---
+++ Baekjoon_Solve/Math/16486.py
@@ -0,0 +1,11 @@
+# 운동장 한 바퀴 (16486번)
+import sys
+input = sys.stdin.readline
+
+d1 = int(input().rstrip())
+d2 = int(input().rstrip())
+pi = 3.141592
+
+playground = d1 * 2 + d2 * 2 * pi
+print(playground)
+
