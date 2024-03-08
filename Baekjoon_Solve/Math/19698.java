21423c4aa3ae4eaac34c2d32ca182bfde18a35a7
siwon-park <zow777@naver.com>
2024-03-08 21:47:22 +0900

    ps: 헛간 청약 (19698번) 풀이 완료

---
+++ Baekjoon_Solve/Math/19698.java
@@ -0,0 +1,20 @@
+// 헛간 청약 (19698번)
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int W = Integer.parseInt(st.nextToken());
+		int H = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken());
+		
+		System.out.println(Math.min(N, (W / L) * (H / L)));
+	}
+}
+
