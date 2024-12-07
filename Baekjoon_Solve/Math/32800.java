1f68ae7d4f2659e711bd1246c46fe44503b68bf7
siwon-park <zow777@naver.com>
2024-12-07 19:35:23 +0900

    ps: Bus Assignment (32800번) 풀이 완료

---
+++ Baekjoon_Solve/Math/32800.java
@@ -0,0 +1,27 @@
+// Bus Assignment (32800번)
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		int _max = 0;
+		int cur = 0;
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			cur = cur - a + b;
+			_max = Math.max(_max, cur);
+		}
+		
+		System.out.println(_max);
+		
+	}
+}
+
