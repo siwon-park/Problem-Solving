b61ef47de81a935f614a7e9a283a3f6e8e9b7d30
siwon-park <zow777@naver.com>
2025-02-13 23:13:54 +0900

    ps: Conveyor Belt Sushi (32326번) 풀이 완료

---
+++ Baekjoon_Solve/Math/32326.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+# Conveyor Belt Sushi (32326번)
+r = int(input().rstrip())
+g = int(input().rstrip())
+b = int(input().rstrip())
+
+print(r * 3 + g * 4 + b * 5)
+
